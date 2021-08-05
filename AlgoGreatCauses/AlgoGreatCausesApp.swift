b23f65042bfa90b2a8b695c140129a05e460a15f
3dlifeStudio <mjfernandez3d@gmail.com>
2021-08-05 01:10:47 +0100

    Initial Commit

---
+++ AlgoGreatCauses/AlgoGreatCausesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoGreatCausesApp.swift
+//  AlgoGreatCauses
+//
+//  Created by Mario Fernandes on 05/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AlgoGreatCausesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlgoGreatCauses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlgoGreatCauses
+//
+//  Created by Mario Fernandes on 05/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
