2cc9d38dbdb79cdb862fb3eda04ed12a17a1b6bb
3dlifeStudio <mjfernandez3d@gmail.com>
2021-08-17 12:20:13 +0100

    DryRun update

--- AlgoGreatCauses/data.swift
+++ AlgoGreatCauses/data.swift
@@ -34,19 +34,19 @@ let algoData = [
          mnemonic1: "dork fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork check",
          mnemonic2: "diet say deposit aerobic wisdom twist alarm creek trap try ahead rapid lucky aisle hawk tissue weather saddle mobile elegant guitar usual garlic abstract pupil",
          mnemonic3: "work fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork"),
-    
+
     data(categories: "Health", imageName: "health", imageName1: "photoReceiver1", imageName2: "photoReceiver2", imageName3: "photoReceiver3", title: "Health", algoValue: "100000",
          mnemonic: "fork ork fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork social",
          mnemonic1: "dork fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork check",
          mnemonic2: "diet say deposit aerobic wisdom twist alarm creek trap try ahead rapid lucky aisle hawk tissue weather saddle mobile elegant guitar usual garlic abstract pupil",
          mnemonic3: "work fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork"),
-    
+
     data(categories: "Housing", imageName: "housing", imageName1: "photoReceiver1", imageName2: "photoReceiver2", imageName3: "photoReceiver3", title: "Housing", algoValue: "100000",
          mnemonic: "fork ork fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork social",
          mnemonic1: "dork fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork check",
          mnemonic2: "diet say deposit aerobic wisdom twist alarm creek trap try ahead rapid lucky aisle hawk tissue weather saddle mobile elegant guitar usual garlic abstract pupil",
          mnemonic3: "work fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork"),
-    
+
     data(categories: "Biotechnology", imageName: "Biotechnology", imageName1: "photoReceiver1", imageName2: "photoReceiver2", imageName3: "photoReceiver3", title: "Biotechnology", algoValue: "100000",
          mnemonic: "fork ork fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork social",
          mnemonic1: "dork fork fork fork fork fork fork fork fork fork fork fork fork v fork fork fork fork fork fork fork fork fork fork fork fork check",
--- AlgoGreatCauses/donationUI.swift
+++ AlgoGreatCauses/donationUI.swift
@@ -9,6 +9,9 @@ import SwiftUI
 import swift_algorand_sdk
 
 struct donationUI: View {
+    // MARK: -  Balance
+    @State var balanceAfter:String = ""
+    @State var balanceBefore:String = ""
     
     @State private var assetRewardState = false
     @State var isLoading = false
@@ -22,7 +25,7 @@ struct donationUI: View {
     
     // MARK: - PureStake API
     var ALGOD_API_ADDR="https://testnet-algorand.api.purestake.io/ps2"
-    var ALGOD_API_TOKEN="Gs——————————————————"
+    var ALGOD_API_TOKEN="G—————————————————l5"
     var ALGOD_API_PORT=""
     
     // MARK: - data
@@ -182,6 +185,10 @@ struct donationUI: View {
             
                 Button(action: {
                     self.tealCompile()
+                    self.getAccountBalance(){amount in
+                        print(amount)
+                        balanceBefore = String(amount)
+                    }
                 }) {
                     Image("compileBtn")
                         .padding(.bottom)
@@ -200,7 +207,8 @@ struct donationUI: View {
                     VStack(){
                         if self.transactionIsShowing == true {
                             Image(transactionSuccedImage)
-                            
+                            Text("Account Balance Before Transaction: \(balanceBefore)")
+                            Text("Account Balance After Transaction: \(balanceAfter)")
                             Button("Dismiss",
                                    action: {  self.assetRewardState.toggle() })
                             
@@ -213,7 +221,7 @@ struct donationUI: View {
                     }
                 }
                 .disabled(compileVerified == false)
-                .opacity(compileVerified == false ? 0.6 : 1)
+                .opacity(compileVerified == false ? 0.0 : 1)
             }
                 // MARK: - Compile
                 Text(complieHash)
@@ -238,6 +246,26 @@ struct donationUI: View {
         }
         .edgesIgnoringSafeArea(.top)
     }
+    
+    // MARK: - function Balance
+
+    func getAccountBalance(functionToCall: @escaping (String)->Void){
+        let algodClient=AlgodClient(host: ALGOD_API_ADDR, port: ALGOD_API_PORT, token: ALGOD_API_TOKEN)
+        algodClient.set(key: "X-API-KeY")
+        let mnemonic = EnvioData.mnemonic
+        let account = try! Account(mnemonic)
+        algodClient.accountInformation(address: account.address.description).execute(){accountInformationResponse in
+            if(accountInformationResponse.isSuccessful){
+                print("\(accountInformationResponse.data!.amount!)")
+                functionToCall("\(accountInformationResponse.data!.amount!)")
+            }else{
+                functionToCall("\(String(describing: accountInformationResponse.errorDescription))")
+                
+            }
+        }
+        
+    }
+    
     // MARK: - function Loading
     func startHolding(){
         isLoading = true
@@ -279,13 +307,26 @@ struct donationUI: View {
         return data
     }
     
+    // MARK: - function tealCompile Dryrun
+    
+    public static func loadSampleTeal2()  -> [Int8] {
+        let configURL = Bundle.main.path(forResource: "compile.teal", ofType: "txt")
+        let contensts = try! String(contentsOfFile: configURL!.description)
+        let jsonData = contensts.data(using: .utf8)!
+
+        let  data = CustomEncoder.convertToInt8Array(input: Array(jsonData))
+        print(data)
+        return data
+       }
+    
+    
     // MARK: - function transaction
     func createtransactions(){
         
         let algodClient=AlgodClient(host: ALGOD_API_ADDR, port: ALGOD_API_PORT, token: ALGOD_API_TOKEN)
         algodClient.set(key: "X-API-KeY")
         
-        var stxns:[SignedTransaction] = Array()
+       
         // MARK: - data.swift
         let mnemonic = EnvioData.mnemonic
         let mnemonic1 = EnvioData.mnemonic1
@@ -308,6 +349,17 @@ struct donationUI: View {
                 return;
             }
         
+            // MARK: - DryrunRequest
+            var sources:[DryrunSource] = Array()
+            var stxns:[SignedTransaction] = Array()
+            let source:[Int8] = donationUI.loadSampleTeal2()
+            let dryRunSource = DryrunSource()
+            let dryRunRequest = DryrunRequest()
+            dryRunSource.fieldName =  "approv"
+            dryRunSource.source =  String(data: Data(CustomEncoder.convertToUInt8Array(input:  source)),encoding: .utf8)!
+            dryRunSource.txnIndex = 01
+            sources.append(dryRunSource)
+            
             let program:[Int8] = CustomEncoder.convertToInt8Array(input: CustomEncoder.convertBase64ToByteArray(data1: "ASABASI="))
             
             let lsig = try! LogicsigSignature(logicsig: program)
@@ -353,16 +405,37 @@ struct donationUI: View {
                 transactions1[i].assignGroupID(gid: gid)
                 signedTransactions[i]=Account.signLogicsigTransaction(lsig: lsig,tx: transactions1[i])
                 stxns.append(signedTransactions[i]!)
+                dryRunRequest.sources = sources
+                dryRunRequest.txns = stxns
             }
 
+            _ = CustomEncoder.encodeToJson(obj: dryRunRequest)
             var encodedTrans:[Int8]=Array()
             for i in 0..<signedTransactions.count{
                 encodedTrans = encodedTrans+CustomEncoder.encodeToMsgPack(signedTransactions[i])
             }
-                    algodClient.rawTransaction().rawtxn(rawtaxn: encodedTrans).execute(){
-                       response in
-                        if(response.isSuccessful){
-                            self.waitForTransaction(txId:response.data!.txId)
+            algodClient.tealDryRun().request(request: dryRunRequest).execute(){ requestResponse in
+//
+                        if(requestResponse.isSuccessful){
+                            print(requestResponse.data!.toJson() as Any)
+                            
+                            algodClient.rawTransaction().rawtxn(rawtaxn: encodedTrans).execute(){
+                                                  response in
+                                if(response.isSuccessful){
+                                   
+                                    print(response.data!.txId)
+                                    print("foi criado")
+                                    self.waitForTransaction(txId:response.data!.txId)
+                     
+                                    
+                                }else{
+                                    print(response.errorDescription!)
+                                    print("Failed")
+                                }
+                            }
+                           
+                           
+
                             print("Created")
                         }else{
                             print("Failed")
@@ -389,6 +462,9 @@ struct donationUI: View {
                         DispatchQueue.main.async {
                             print(transactionIsShowing)
                             self.assetRewardState = true
+                            self.getAccountBalance(){amount in
+                                balanceAfter = String(amount)
+                            }
                         }
                     }
                 }else{
